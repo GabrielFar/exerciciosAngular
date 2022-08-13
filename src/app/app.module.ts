f231fea716d8679a1315f582506d5cf29f64ae52
Gabriel Farina <gabrielfarina2358@gmail.com>
2022-08-12 22:04:06 -0300

    add componentes e começo html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AutentificacaoComponent } from './autentificacao/autentificacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AutentificacaoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/autentificacao/autentificacao.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-autentificacao', 
+    templateUrl: './autentificacao.component.html', 
+    styleUrls: ['./autentificacao.component.css']
+})
+
+export class AutentificacaoComponent{
+
+}
