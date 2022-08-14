69a5463059c97b5a0b3d0f60e68ac3c223d35520
Gabriel Farina <gabrielfarina2358@gmail.com>
2022-08-14 15:49:10 -0300

    add parte lógica

--- src/app/autentificacao/autentificacao.component.ts
+++ src/app/autentificacao/autentificacao.component.ts
@@ -7,5 +7,36 @@ import { Component } from "@angular/core";
 })
 
 export class AutentificacaoComponent{
+    email: string = "";
+    senha: string = "";
+    msn: string = "";
+    contaTentativasSenha: number = 0
+    msnStyle: string = "d-flex justify-content-center"
+    isBloqueado: boolean = false;
 
+    public login(){
+        
+        if(this.senha == "Trocar@123" && this.email == "XPTO-21" && this.isBloqueado == false){
+            this.msn = "Logado!"
+            this.contaTentativasSenha = 0
+            this.msnStyle = "d-flex justify-content-center text-success"
+
+        } else if (this.contaTentativasSenha >= 3) {
+            this.msn = "Usuário Bloqueado!"
+            this.msnStyle = "d-flex justify-content-center text-danger"
+            this.isBloqueado = true
+
+        } else if(this.email !== "XPTO-21"){
+            this.msn = "Acesso negado, usuário incorreto"
+            this.msnStyle = "d-flex justify-content-center text-danger"
+            this.contaTentativasSenha++
+
+        } else if(this.senha !== "Trocar@123"){
+            this.msn = "Acesso negado, senha incorreta"
+            this.msnStyle = "d-flex justify-content-center text-danger"
+            this.contaTentativasSenha++
+
+        }
+        console.log(this.contaTentativasSenha);
+    }
 }
